ac468e9ddfb741e86da1950b174b8f3d86c8cf29
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-11-19 03:59:46 +0600

    counting the touples

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print('Working main.py')
+# print('Working main.py')
+
+#touple : a = (1,2,3,4,5,4)
+
+#touples are immutable
+# a = [1,2,3,4,5,4]
+# print(a)
+# a[0]= 12 
+# print(a)
+#now in the list we had that option to change at any time at any index but in touple it's not possible
+
+#you can have duplicates 
+#set and touples are ordered values 
+#it has heterogenious nature i can store int , float , str etc. 
+
+
+# b = [1,3,4,5,3,2,1,4,5,6]
+# for i in b : 
+#     print(b)
+    
+# for i in range(len(b)): 
+#          print(b[i])
+         
+#touples have 2 mentods only : index finding and counting 
+""" t=(5,2,3,4,6,4,42,3,5,6,32,3)
+index = t.index(9) 
+count_5 = t.count(5)"""
+
