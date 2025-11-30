4ada2198678d779374019924f30082ab93aad2ed
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-11-30 23:34:23 +0600

    creating in absence of that file.

--- DSA_python/filehandeling.py
+++ DSA_python/filehandeling.py
@@ -6,12 +6,18 @@
 
 # print(p.read())
 
-import os
-print(os.listdir('.'))  # See all files in current directory
+# import os
+# print(os.listdir('.'))  # See all files in current directory
 
 # p = open('filehandeling.py')
 
 # print(p.read())
 
-p = open('filehandeling.py')
-print(p.read())
+# p = open('filehandeling.py')
+# print(p.read())
+
+## if there isn't any file on that name and if it's not found then it will create by this name to that location
+
+#file handeling 
+
+r = open('tanju_superman.txt','w') #here w means it will create if there is nothing found based on that name !
