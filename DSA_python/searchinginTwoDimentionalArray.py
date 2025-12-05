945f46fef8478661d324eeae6f276144b27c97ca
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-12-05 17:59:52 +0600

    serarching an number in to a multi dimentional array

---
+++ DSA_python/searchinginTwoDimentionalArray.py
@@ -0,0 +1,17 @@
+
+import numpy as np 
+
+
+my_new_array = np.array([[1,2,3,4,5,6],[11,21,31,41,51,61],[12,22,32,42,52,62],[14,24,34,44,54,64]])
+
+
+
+def serarching(array,value): 
+        for i in range(len(array)):
+             for j in range(len(array[0])): 
+                 if array[i][j] == value: 
+                     return True
+                 else: 
+                      False
+            
+print(serarching(my_new_array,51))
