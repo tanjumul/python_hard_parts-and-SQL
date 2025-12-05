76713e665917b3e55624d9b6e1406cc6375320f5
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-12-05 18:14:31 +0600

    optimized v2

--- DSA_python/searchinginTwoDimentionalArray.py
+++ DSA_python/searchinginTwoDimentionalArray.py
@@ -2,7 +2,10 @@
 import numpy as np 
 
 
-my_new_array = np.array([[1,2,3,4,5,6],[11,21,31,41,51,61],[12,22,32,42,52,62],[14,24,34,44,54,64]])
+my_new_array = np.array ([[1,2,3,4,5,6],
+                         [11,21,31,41,51,61],
+                         [12,22,32,42,52,62],
+                         [14,24,34,44,54,64]])
 
 
 
@@ -10,9 +13,9 @@ def serarching(array,value):
         for i in range(len(array)):
              for j in range(len(array[0])): 
                  if array[i][j] == value: 
-                     return value
+                     return f'value is located in the {i} {j} and the value you are looking {value}'
         else: 
                       
-                      return print('The number you are looking here in the array doesnt exitst')
+                      return 'The number you are looking here in the array doesnt exitst'
                       
-print(serarching(my_new_array,221))
+print(serarching(my_new_array,51))
