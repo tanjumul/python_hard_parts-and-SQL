73ac8ff3718b3356f630f496adbaea2d25ba5979
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-12-06 18:25:09 +0600

    shifted from numpy to list but not working

--- DSA_python/delete_arrays.py
+++ DSA_python/delete_arrays.py
@@ -1,19 +1,26 @@
 import numpy as np
 
 mynew_array = np.array([[81,82,83,84],[71,72,73,74],[61,62,63,64],[11,22,33,44]])
-found = False
-def deletearray(array,delNum): 
-    for i in range(len(array)): 
-        for j in range (len(array[0])):
-            if array[i][j] == delNum: 
-                array[i].pop(j)
-                found = True
-                break 
-        if found : 
-            break
-        if not found: 
-            print('sorry your number couldnt found in the array ')
+
+# def deletearray(array,delNum): 
+#     arr_list = array.tolist()
+#     found = False
+#     for i in range(len(arr_list)): 
+#         for j in range (len(arr_list[0])):
+#             if arr_list[i][j] == delNum: 
+#                 arr_list[i].pop(j)
+#                 return np.array(arr_list)
         
 
-print(deletearray(mynew_array,61))
+# print(deletearray(mynew_array,61))
         
+def deletearray(array, delNum): 
+    arr_list = array.tolist()
+    for i in range(len(arr_list)): 
+        for j in range(len(arr_list[0])):
+            if arr_list[i][j] == delNum: 
+                arr_list[i].pop(j)
+                return np.array(arr_list)
+    print('Number not found')
+    return array
+print(deletearray(mynew_array ,61))
